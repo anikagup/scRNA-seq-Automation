dddbf264157bb59d4a5d339e9f86daf552030444
Erik Noyman <eriknoyman@MacBook-Pro.local>
2025-05-03 19:17:32 -0700

    uopdated ML functions to incorperate model confidence and correctly assign np.NAN to missing genes

--- src/ML/workflow.py
+++ src/ML/workflow.py
@@ -25,27 +25,34 @@ def align_anndata_to_genes(adata: AnnData, expected_genes: list) -> AnnData:
     # Return aligned AnnData
     aligned = AnnData(X=X_final, obs=adata.obs.copy())
     aligned.var_names = expected_genes
-    return aligned
+    return aligned, missing_genes
 
-def predict_labels(adata, ML_data):
+def predict_labels(adata, ML_data, confidence_threshold=0.9):
     # Load trained gene list (from feature importances CSV)
-    HVG_df = pd.read_csv("src/ML/XGB_2kHVG_feature_importances.csv")
+    HVG_list = pd.read_csv("src/ML/XGB_classifier_V3_gene_names.csv").iloc[0].tolist()
     
-    # # Keep only genes with non-zero importance
-    # HVG_list = HVG_df[HVG_df["importance"] > 0]["gene"].tolist()
-    
-    HVG_list = HVG_df["gene"].tolist()
-
-
     # Align ML_data to expected gene list
-    ML_aligned = align_anndata_to_genes(ML_data, HVG_list)
+    ML_aligned, missing_genes = align_anndata_to_genes(ML_data, HVG_list)
 
     # Load XGBoost classifier
-    classifier = load('src/ML/XGB_classifier_2kHVG.joblib')
+    classifier = load('src/ML/XGB_classifier_V3.joblib')
 
     # Predict using aligned data
     X_input = ML_aligned.X.toarray() if hasattr(ML_aligned.X, "toarray") else ML_aligned.X
+    
+    #Get index of missing genes
+    missing_gene_indices = [ML_aligned.var_names.get_loc(gene) for gene in missing_genes]
+    
+    # Set missing genes to NaN
+    X_input[:, missing_gene_indices] = np.nan
+    
+    #predict usig aligned data
     predictions = classifier.predict(X_input)
+
+    # Get probabilities for each class
+    probability = classifier.predict_proba(X_input)
+
+    confidence_scores = np.max(probability, axis=1)
 
     # Map numeric predictions to labels
     label_dict = {
@@ -54,9 +61,19 @@ def predict_labels(adata, ML_data):
         2: 'Early (Stage I/II)',
         3: 'Advanced (Stage III/IV)'
     }
-    word_labels = [label_dict[num_label] for num_label in predictions]
+
+    raw_labels = [label_dict[p] for p in predictions]
+
+    # Step 4: Apply confidence threshold
+    final_labels = [
+        raw_label if confidence >= confidence_threshold else "Unknown"
+        for raw_label, confidence in zip(raw_labels, confidence_scores)
+    ]
 
     # Add predictions to output AnnData object
-    adata.obs["Classifier_predictions"] = word_labels
+    adata.obs["Classifier_predictions"] = raw_labels
+    adata.obs["Classifier_prediction_probablility"] = confidence_scores
+    adata.obs["Classifier_FINAL"] = final_labels
+
 
     return adata
